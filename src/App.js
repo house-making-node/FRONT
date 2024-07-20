dc497e7b2ae209853fcce331ca1e5398fc3f72ab
YoungEE <134985583+Seonyoung33@users.noreply.github.com>
2024-07-20 16:08:45 +0000

    ShareLetter_1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./component/header/Navbar";
 import MainPage from "./page/MainPage";
 import QuestionBoard from "./component/inquiry/questionBoard";
+import ShareLetterPage from './page/ShareLetterPage';
+// import ShareLetterComplete from './page/ShareLetterComplete';
+
 
 function App() {
   return (
@@ -11,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/faq" element={<QuestionBoard />} />
+          <Route path="/shareletter" element={<ShareLetterPage />} />
         </Routes>
       </BrowserRouter>
     </div>
