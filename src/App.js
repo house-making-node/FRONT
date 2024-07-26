746d828c1b69ff949fd474818ac3903a67597abf
ssxrxbx <123061283+ssxrxbx@users.noreply.github.com>
2024-07-26 13:08:25 +0900

    adding consultingPages in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./component/header/Navbar";
 import MainPage from "./page/MainPage";
 import QuestionBoard from "./component/inquiry/questionBoard";
+import Step1Page from './component/consulting/step1Page';
+import Step2Page from './component/consulting/step2Page';
+import Step3Page from './component/consulting/step3Page';
+import Step4Page from './component/consulting/step4Page';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/faq" element={<QuestionBoard />} />
+          <Route path="/consulting/step1Page" element={<Step1Page />} />
+          <Route path="/consulting/step2Page" element={<Step2Page />} />
+          <Route path="/consulting/step3Page" element={<Step3Page />} />
+          <Route path="/consulting/step4Page" element={<Step4Page />} />
         </Routes>
       </BrowserRouter>
     </div>
