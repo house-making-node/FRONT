8e2f7073989a9484fd1e248de3c9bcaf2f480569
Goguma <chae8246@gmail.com>
2024-08-21 00:00:40 +0900

    공유레터상태관리오류해결

--- src/page/Thumbnail1.js
+++ src/page/Thumbnail1.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
 import axios from "axios";
 
@@ -12,9 +13,13 @@ function Thumbnail1({ src, description, onClick, id, publicationDate }) {
     async function loadBookmarkStatus() {
       try {
         const response = await axios.get(
-          "http://3.36.240.5:3000/user/1/share_letters/scraps"
+          "http://3.36.240.5:3000/user/1/share_letters/scraps",
+          {
+            params: {
+              user_id: 1, // 현재 사용자 IDX
+            },
+          }
         );
-
         console.log("정보:", response.data);
   
         // response.data.result가 배열인지 확인하고, 배열이 아닌 경우 빈 배열로 설정
@@ -40,7 +45,6 @@ function Thumbnail1({ src, description, onClick, id, publicationDate }) {
           console.error('북마크 상태를 불러오는 데 실패했습니다:', error.message);
         }
         setDataLoaded(true);
-
       }
     }
   
@@ -50,16 +54,18 @@ function Thumbnail1({ src, description, onClick, id, publicationDate }) {
 
   const handleBookmarkClick = async (e) => {
     e.stopPropagation();
+    console.log("북마크 클릭됨");
+
+    console.log("letter_id:", id);
+    // console.log('id:', id);
+    // console.log('num:', num);
 
     try {
+      let response;
       const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
         timeout: 3000,
       };
 
-      let response;
       if (isBookmarked) {
         response = await axios.delete(
           "http://3.36.240.5:3000/share_letters/scrapX",
@@ -76,31 +82,26 @@ function Thumbnail1({ src, description, onClick, id, publicationDate }) {
         );
       }
 
-
       if (response.data.isSuccess) {
-
         setIsBookmarked(!isBookmarked);
         setPopupMessage(isBookmarked ? "저장 취소되었습니다" : "저장되었습니다");
+        console.log("Response:", response.data);
+        setShowPopup(true);
       } else {
-
         setPopupMessage(`오류가 발생했습니다: ${response.data.message}`);
         setShowPopup(true);
       }
-
-      setShowPopup(true);
     } catch (error) {
-
       console.error("북마크 처리 중 오류 발생:", error);
       setPopupMessage("오류가 발생했습니다. 다시 시도해주세요.");
       console.log('에러:',error.message)
-      
       setShowPopup(true);
     }
   };
 
   const handleConfirmClick = (e) => {
-    e.stopPropagation();
     setShowPopup(false);
+    e.stopPropagation();
   };
 
   return (
@@ -135,4 +136,5 @@ function Thumbnail1({ src, description, onClick, id, publicationDate }) {
 }
 
 export default Thumbnail1;
+
 
